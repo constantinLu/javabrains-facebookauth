00728a7ec175b40f343a3c53c9056b5fe1277e06
gizet <lunguucatalin@gmail.com>
2020-01-21 11:24:54 +0200

    [JavaBrains] FacebookAuth

---
+++ src/test/java/com/gizet/facebooklogin/FacebookloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gizet.facebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
